277feb69cdc6c709f461f37dda8009d77a1e42f4
chouaib <zaidaneb@gmail.com>
2024-01-22 10:17:06 +0100

    fixed from errors

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * print_python_bytes - print some basic info about Python bytes objects
+ * print_python_bytes - print some basic info about python byte
  * @p: python object
  * Return: nothing
  **/
@@ -9,7 +9,7 @@ void print_python_bytes(PyObject *p)
 {
 }
 /**
- * print_python_float - print Python float
+ * print_python_float - print floatin python
  * @p: python float
  * Return: nothing
  **/
@@ -17,7 +17,7 @@ void print_python_float(PyObject *p)
 {
 }
 /**
- * print_python_list - print some basic info about Python lists
+ * print_python_list - print basic info
  * @p: python object
  * Return: nothing
  **/
