e8ab42eb78f74b8acbb450e16ed764e53fc5f1ef
ch3ayba <zaidaneb@gmail.com>
2024-05-29 12:17:31 +0200

    script 7

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,6 @@
+// a script that updates the text of the header
+// to New Header when the user clicks on DIV
+
+$('DIV#update_header').click(function () {
+    $('header').html('New Header!!!');
+});
